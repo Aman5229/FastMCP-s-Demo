1fc216d323ff8e11d80e3743dd76b3bb8fbeeda1
aman5229 <amanpatel5229@gmail.com>
2026-07-02 02:38:52 +0530

    setup db

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from database import engine
+import model
+
+model.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/")
+
+def home():
+  return{
+    "Message": "Blog api "
+  }
