1f538fdfc17fecd5ddda07d4258d3c60ceb4c034
aman5229 <amanpatel5229@gmail.com>
2026-07-02 03:23:53 +0530

    Added CRUD for blog

--- database.py
+++ database.py
@@ -5,12 +5,13 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./blog.db")
 
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set. Add it to your .env file.")
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(bind=engine)
 
--- main.py
+++ main.py
@@ -1,14 +1,80 @@
-from fastapi import FastAPI
-from database import engine
-import model
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal
+import models, schemas
 
-model.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
+#db dependency
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
 
+#home
+@app.get("/")
 def home():
   return{
     "Message": "Blog api "
   }
+
+# Create Blog
+@app.post("/blogs", response_model=schemas.BlogResponse)
+def create_blog(blog:schemas.BlogCreate, db:Session = Depends(get_db)):
+  new_blog = models.Blog(
+    title = blog.title,
+    content = blog.content
+  )
+  db.add(new_blog)
+  db.commit()
+  db.refresh(new_blog)
+
+  return new_blog
+
+
+# Fetch Blogs
+@app.get("/blogs",response_model=list[schemas.BlogResponse])
+def get_blogs(db: Session = Depends(get_db)):
+  return db.query(models.Blog).all()
+
+
+# Fetch by Id
+@app.get("/blogs/{id}", response_model=schemas.BlogResponse)
+def get_blog(id:int, db: Session = Depends(get_db)):
+  blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+  if not blog:
+    raise HTTPException(status_code=404, detail="Blog not found")
+  return blog
+
+
+# Update blog
+@app.put("/blogs/{id}", response_model=schemas.BlogResponse)
+def update_blog(id:int, blog: schemas.BlogCreate, db: Session = Depends(get_db)):
+  existing_blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+  if not existing_blog:
+    raise HTTPException(status_code=404, detail="Blog not found")
+
+  existing_blog.title = blog.title
+  existing_blog.content = blog.content
+
+  db.commit()
+  db.refresh(existing_blog)
+
+  return existing_blog
+
+# Delete Blog
+@app.delete("/blogs/{id}")
+def delete_blog(id:int, db: Session = Depends(get_db)):
+  blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+  if not blog:
+     raise HTTPException(status_code=404, detail="Blog not found")
+
+  db.delete(blog)
+  db.commit()
+
+  return {"Message": "Blog deleted"}
+
